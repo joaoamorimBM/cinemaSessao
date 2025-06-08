8faf5e3775d6f05a7801bbf42165acc4e2354570
Joao Pedro <joaopedroamorimbm@gmail.com>
2025-06-08 09:55:40 -0300

    listando nome dos filmes

--- src/controllers/sessaoController.ts
+++ src/controllers/sessaoController.ts
@@ -38,12 +38,44 @@ export const criarSessao = async (req: Request, res: Response) => {
   }
 };
 
+// export const listarSessoes = async (req: Request, res: Response) => {
+//   try {
+//     console.log('Iniciando busca de sessões...');
+//     const sessoes = await sessaoRepository.findAll();
+//     console.log(`Encontradas ${sessoes.length} sessões`);
+//     res.status(200).json(sessoes);
+//   } catch (error) {
+//     console.error('Erro detalhado ao listar sessões:', error);
+//     if (error instanceof Error) {
+//       res.status(500).json({ 
+//         message: 'Erro ao listar sessões', 
+//         error: error.message,
+//         stack: process.env.NODE_ENV === 'development' ? error.stack : undefined
+//       });
+//     } else {
+//       res.status(500).json({ message: 'Erro ao listar sessões', error: 'Erro desconhecido' });
+//     }
+//   }
+// };
+
 export const listarSessoes = async (req: Request, res: Response) => {
   try {
     console.log('Iniciando busca de sessões...');
     const sessoes = await sessaoRepository.findAll();
-    console.log(`Encontradas ${sessoes.length} sessões`);
-    res.status(200).json(sessoes);
+
+    // mapeia cada sessão para uma Promise que busca os detalhes do filme
+    const sessoesComFilmes = await Promise.all(
+      sessoes.map(async (sessao) => {
+        const filme = await buscarFilmePorId(sessao.filmeId);
+        return {
+          ...sessao.toObject(),
+          filme: filme || { message: "Detalhes do filme não disponíveis." }
+        }
+      })
+    )
+
+    console.log(`Encontradas ${sessoesComFilmes.length} sessões`);
+    res.status(200).json(sessoesComFilmes);
   } catch (error) {
     console.error('Erro detalhado ao listar sessões:', error);
     if (error instanceof Error) {
@@ -54,8 +86,8 @@ export const listarSessoes = async (req: Request, res: Response) => {
       });
     } else {
       res.status(500).json({ message: 'Erro ao listar sessões', error: 'Erro desconhecido' });
-    }
-  }
+    }
+  }
 };
 
 export const buscarSessaoPorId = async (req: Request, res: Response) => {
